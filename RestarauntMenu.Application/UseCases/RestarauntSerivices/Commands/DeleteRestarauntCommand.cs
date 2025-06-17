c6e150110e3a303a91102183c25296f751c60bd5
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2025-06-17 12:17:24 +0500

    adding restaraunt Services

---
+++ RestarauntMenu.Application/UseCases/RestarauntSerivices/Commands/DeleteRestarauntCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestarauntMenu.Application.UseCases.RestarauntSerivices.Commands
+{
+    internal class DeleteRestarauntCommand
+    {
+    }
+}
