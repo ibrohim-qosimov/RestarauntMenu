f20cfe3ef72b07ca6976bca276739811ca83a256
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2025-06-22 14:39:10 +0500

    changing in restaurant model

--- RestarauntMenu.Domain/Entities/User.cs
+++ RestarauntMenu.Domain/Entities/User.cs
@@ -1,14 +1,20 @@
-﻿using RestarauntMenu.Domain.Enums;
+﻿using RestarauntMenu.Domain.DTOs;
+using RestarauntMenu.Domain.Enums;
 
 namespace RestarauntMenu.Domain.Entities
 {
     public class User
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
+        public long Id { get; set; }
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
         public UserRole Role { get; set; } = UserRole.User;
         public string PasswordHash { get; set; }
+
+        /// <summary>
+        ///  Admin uchun restarant qoshiladi, restarantni boshqarish uchun , restaraunt create vaqtidaaaa
+        /// </summary>
+        public Restaraunt? Restaraunt { get; set; }
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
