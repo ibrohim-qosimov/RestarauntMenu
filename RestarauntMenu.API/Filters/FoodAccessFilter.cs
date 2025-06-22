15b630145778679bad46cd4cf64dddfb14c7f5a1
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2025-06-22 14:37:23 +0500

    adding filters

---
+++ RestarauntMenu.API/Filters/FoodAccessFilter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using RestarauntMenu.Application.UseCases.AuthServices.AccesssAuthorizationCehckerService;
+using System.Security.Claims;
+
+namespace RestarauntMenu.API.Filters
+{
+    public class FoodAccessAttribute : TypeFilterAttribute
+    {
+        public FoodAccessAttribute() : base(typeof(FoodAccessFilter)) { }
+    }
+
+    public class FoodAccessFilter : IAsyncActionFilter
+    {
+        private readonly IAccessAuthorizationChecker _accessChecker;
+
+        public FoodAccessFilter(IAccessAuthorizationChecker accessChecker)
+        {
+            _accessChecker = accessChecker;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var userId = long.Parse(context.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+            if (context.ActionArguments.TryGetValue("id", out var idObj) && idObj is long foodId)
+            {
+                var hasAccess = await _accessChecker.HasAccessToFoodAsync(foodId, userId);
+                if (!hasAccess)
+                {
+                    context.Result = new ForbidResult("Access denied");
+                    return;
+                }
+            }
+            await next();
+        }
+    }
+}
